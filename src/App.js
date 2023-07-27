fdc119baab19dff6401f7a84ff91ca72ddc48951
David Chaney <david.ray.chaney@gmail.com>
2023-07-27 09:52:51 -0500

    add App.js

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import BarcodeScanner from './components/BarcodeScanner';
+import './App.css';
+
+function App() {
+  const [barcode, setBarcode] = useState('n/a')
+  const [startScanning, setStartScanning] = useState(false)
+  const [buttonText, setButtonText] = useState('Start Scanning')
+
+  function handleButtonClick(event){
+    event.preventDefault();
+    setStartScanning(!startScanning);
+    if (!startScanning){
+      setButtonText('Stop Scanning')
+    } else {
+      setButtonText('Start Scanning')
+    }
+  }
+
+  return (
+    <>
+      <h1>Scanning QR/barcodes...</h1>
+      <button onClick={handleButtonClick}>{buttonText}</button>
+      <h2>Code scanned: {barcode}</h2>
+      {
+        startScanning? 
+          <>
+            <BarcodeScanner setBarcode={setBarcode}/>
+            <video id="scanning-video-frame"/>
+          </>
+        : null
+      }
+    </>
+  );
+}
+
+export default App;
