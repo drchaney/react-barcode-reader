a802a8bd26fd7f2b8162e0ee2862710c2218dc5e
David Chaney <david.ray.chaney@gmail.com>
2023-07-27 13:54:24 -0500

    add comments to BarcodeScanner

--- src/components/BarcodeScanner.js
+++ src/components/BarcodeScanner.js
@@ -4,28 +4,25 @@ import { BrowserMultiFormatReader } from '@zxing/library';
 export default function BarcodeScanner({ setBarcode }){
   const barcodeReader = new BrowserMultiFormatReader();
 
-  function handleScan(result){
-    if (result) {
-      setBarcode(result.text);
-    }
-  };
 
+  // decodeOnceFromVideoDevice method needs two inputs: 
+  // - The video deviceId
+  // - The ID of the video element to show live video (optional)
+  // The first input is 'undefined' because 'getVideoInputDevices' is depreciated
+  // 'scanning-video-frame' is the ID of the <video> element in src/App.js
   useEffect(() => {
     async function readBarcode() {
       try {
-        // decodeOnceFromVideoDevice method needs two inputs: the video device, and the id of the video element to show live video (optional)
-        // The first input is 'undefined' because 'getVideoInputDevices' is depreciated
-        // 'scanning-video-frame' is the id of the video element in App.js
         const result = await barcodeReader.decodeOnceFromVideoDevice(undefined, 'scanning-video-frame');
-        handleScan(result);
+        setBarcode(result.text);
       } catch (error) {
         console.error(error);
       }
     }
     
+    // Execute reset method when BarcodeScanner unmounts so the camera doesn't stay on when the 
+    // component is no longer active ("Stop scanning" button)
     readBarcode();
-      // Execute reset method when BarcodeScanner unmounts so the camera doesn't stay on when the 
-      // component is no longer active ("Stop scanning" button)
     return () => {
       barcodeReader.reset();
     }
