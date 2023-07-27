3c564927c957e6dc8781417ac91390b883b50d85
David Chaney <david.ray.chaney@gmail.com>
2023-07-27 13:35:46 -0500

    add comments to redBarcode

--- src/components/BarcodeScanner.js
+++ src/components/BarcodeScanner.js
@@ -13,6 +13,9 @@ export default function BarcodeScanner({ setBarcode }){
   useEffect(() => {
     async function readBarcode() {
       try {
+        // decodeOnceFromVideoDevice needs two inputs: the video device, and the id of the element to show live video (optional)
+        // The first input is 'undefined' because 'getVideoInputDevices' is depreciated
+        // 'scanning-video-frame' is the id of the video element in App.js
         const result = await barcodeReader.decodeOnceFromVideoDevice(undefined, 'scanning-video-frame');
         handleScan(result);
       } catch (error) {
